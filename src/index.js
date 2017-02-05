547b6ede4ab7c6c8b83ef0997009c3e110171c2c
Mike Skalandunas <mskalandunas@gmail.com>
2017-02-05 01:16:25 -0800

    changes to index and add footer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 'use strict';
 
 import './css/style.css';
-import React from 'react';
-import { render } from 'react-dom';
-import Riverine from './js/components/riverine';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import Riverine from './js/components/riverine';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Riverine/>
-    )
-  }
-}
+// const Root = () => {
+//   return (
 
-render(<App/>, document.querySelector('#root'));
+//   )
+// }
+
+// render(<App/>, document.querySelector('#root'));
