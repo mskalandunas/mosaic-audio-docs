d4439d752e775465f3d207e20e61faecce66d468
Mike Skalandunas <mskalandunas@gmail.com>
2017-02-05 17:44:43 -0800

    update bundle

--- public/bundle.js
+++ public/bundle.js
@@ -21946,7 +21946,6 @@
 	    this.playButton = that.children[1].children[0].children[0];
 	    this.playHead = that.children[2].children[0].children[0];
 	    this.timeline = that.children[2];
-	    console.log(this.timeline);
 	    this.timelineWidth = this.timeline.offsetWidth - this.playHead.offsetWidth;
 
 	    window.addEventListener('mouseup', this.mouseUp, false);
