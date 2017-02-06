f9f3c58987f1b16798f1440b3a3128831bed00a3
Mike Skalandunas <mskalandunas@gmail.com>
2017-02-05 23:38:41 -0800

    alternate css strategy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import './css/style.css';
-import './css/player.css';
 import React from 'react';
 import { render } from 'react-dom';
 import Riverine from './js/components/riverine';
