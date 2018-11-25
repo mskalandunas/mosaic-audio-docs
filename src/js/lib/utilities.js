a7af9b57b5c91af50e989d618ce5dc8fbcdb6c5f
Mike Skalandunas <mskalandunas@gmail.com>
2018-11-24 22:24:52 -0800

    [tests]: add unit tests

--- src/js/lib/utilities.js
+++ src/js/lib/utilities.js
@@ -1,3 +1,41 @@
+export const delimiter = (unit, delimiter = ':') => unit ? delimiter : '';
+
+export const convertDurationToInteger = duration => Math.round(duration);
+
+export const convertHoursToSeconds = hours => hours * 3600;
+
+export const convertMinutesToSeconds = minutes => minutes * 60;
+
+export const createTimeString = duration => {
+    const durationAsInteger = convertDurationToInteger(duration);
+    const hours = getHours(durationAsInteger);
+    const minutes = getMinutes(durationAsInteger, hours);
+    const seconds = getSeconds(durationAsInteger, hours, minutes);
+
+    return formatHours(hours)
+        + delimiter(hours)
+        + formatTimeUnits(minutes)
+        + ':'
+        + formatTimeUnits(seconds);
+};
+
+export const formatHours = hours => hoursExist(hours) ? formatTimeUnits(hours) : '';
+
+export const formatTimeUnits = (timeAmount = 0) =>
+    timeAmount < 10
+        ? '0' + timeAmount
+        : '' + timeAmount;
+
+export const formatResizedPadding = padding =>
+    parseInt(padding.substring(0, padding.length - 2), 10);
+
+export const getHours = duration => Math.floor(duration / 3600);
+
+export const getMinutes = (duration, hours) => Math.floor((duration - (hours * 3600)) / 60);
+
+export const getSeconds = (duration, hours, minutes) =>
+    duration - (convertHoursToSeconds(hours)) - (convertMinutesToSeconds(minutes));
+
 export const handleOffsetParent = originNode => {
     let n = originNode;
     let o = 0;
@@ -10,33 +48,7 @@ export const handleOffsetParent = originNode => {
     return o;
 };
 
-const convertDurationToInteger = duration => parseInt(duration, 10);
-
-const getHours = duration => Math.floor(duration / 3600);
-
-const getMinutes = (duration, hours) => Math.floor((duration - (hours * 3600)) / 60);
-
-const getSeconds = (duration, hours, minutes) => duration - (hours * 3600) - (minutes * 60);
-
-const hoursExist = hours => hours < 10 && hours > 0;
-
-const formatHours = hours => hoursExist(hours) ? hours = '0' + hours + ':' : hours = '';
-
-const formatNonHourTimeUnits = (timeAmount = 0) => timeAmount < 10 ? '0' + timeAmount : timeAmount;
-
-const formatResizedPadding = padding => parseInt(padding.substring(0, padding.length - 2), 10);
-
-export const handleTime = duration => {
-    const durationAsInteger = convertDurationToInteger(duration);
-    const hours = getHours(durationAsInteger);
-    const minutes = getMinutes(durationAsInteger, hours);
-    const seconds = getSeconds(durationAsInteger, hours, minutes);
-
-    return formatHours(hours)
-        + formatNonHourTimeUnits(minutes)
-        + ':'
-        + formatNonHourTimeUnits(seconds);
-};
-
-export const handlePaddingResize = padding =>
+export const handlePaddingResize = (padding = '') =>
     padding === '' ? 0 : formatResizedPadding(padding);
+
+export const hoursExist = (hours = 0) => hours > 0;
